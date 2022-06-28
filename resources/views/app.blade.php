c9d8913e2cf2d6f4abc268df40e7449a10d61352
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-06-28 14:28:30 +0800

    form index [done]

---
+++ resources/views/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>My Contact</title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
+        <!-- Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+    </head>
+    <body>
+
+        {{-- navbar --}}
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <h2>Contact App</h2>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-dark active" aria-current="page" href="#">Company</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        @yield('content') 
+
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,132 @@
+@extends('app')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-title">
+                <div class="d-flex justify-content-between">
+                    <h2 class="mb-0">All Contacts</h2>
+                
+                    <div>
+                        <button class="btn btn-success">
+                            <span><i class="fa fa-plus-circle"></i></span>
+                            Add New
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6"></div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col">
+                                <select name="company" id="company" class="custom-select">
+                                    <option value="all">All Companies</option>
+                                    <option value="company1">Company 1</option>
+                                    <option value="company2">Company 2</option>
+                                    <option value="company3">Company 3</option>
+                                </select>
+                            </div>
+
+                            <div class="col">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username with two button addons" aria-describedby="button-addon4">
+                                    <div class="input-group-append" id="button-addon4">
+                                        <button class="btn btn-outline-secondary" type="button">
+                                            <span><i class="fa fa-refresh"></i></span>
+                                        </button>
+                                        
+                                        <button class="btn btn-outline-secondary" type="button">
+                                            <span><i class="fa fa-search"></i></span>
+                                        </button>
+                                    </div>
+                                  </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- table --}}
+                <table class="table table-striped mt-3">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Company</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Wahyu</td>
+                            <td>Alfatih</td>
+                            <td>Alfatih@gmail.com</td>
+                            <td>Beeyan</td>
+                            <td>
+                                <a href="" class="btn btn-outline-primary btn-circle">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-success btn-circle">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-danger btn-circle">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Wahyu</td>
+                            <td>Alfatih</td>
+                            <td>Alfatih@gmail.com</td>
+                            <td>Beeyan</td>
+                            <td>
+                                <a href="" class="btn btn-outline-primary btn-circle">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-success btn-circle">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-danger btn-circle">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>1</td>
+                            <td>Wahyu</td>
+                            <td>Alfatih</td>
+                            <td>Alfatih@gmail.com</td>
+                            <td>Beeyan</td>
+                            <td>
+                                <a href="" class="btn btn-outline-primary btn-circle">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-success btn-circle">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+
+                                <a href="" class="btn btn-outline-danger btn-circle">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
